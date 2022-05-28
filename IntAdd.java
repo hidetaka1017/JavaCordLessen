7947cf074c4550c389259a60219e1e4922faefef
hidetaka1017 <98687395+hidetaka1017@users.noreply.github.com>
2022-05-28 15:54:44 +0900

    Add files via upload

---
+++ IntAdd.java
@@ -0,0 +1,19 @@
+package algorizum;
+
+import java.util.Scanner;
+
+public class IntAdd {
+
+//	整数の足し算 スペースで区切られた２つの整数を足す
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String S = sc.nextLine();
+		String[] array = S.split(" ");
+		int total = 0;
+		for (String str : array) {
+		   total += Integer.parseInt(str);
+		}
+		System.out.println(total);
+	}
+
+}
