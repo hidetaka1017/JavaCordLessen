6785352b01041bed10bc07e196f445ad3cb85fde
hidetaka1017 <98687395+hidetaka1017@users.noreply.github.com>
2022-05-28 16:19:20 +0900

    Add files via upload

--- IntAdd.java
+++ IntAdd.java
@@ -6,14 +6,23 @@ public class IntAdd {
 
 //	整数の足し算 スペースで区切られた２つの整数を足す
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String S = sc.nextLine();
-		String[] array = S.split(" ");
-		int total = 0;
-		for (String str : array) {
-		   total += Integer.parseInt(str);
+//		Scanner sc = new Scanner(System.in);
+//		String S = sc.nextLine();
+//		String[] array = S.split(" ");
+//		int total = 0;
+//		for (String str : array) {
+//		   total += Integer.parseInt(str);
+//		}
+//		System.out.println(total);
+		
+		Scanner scanner = new Scanner(System.in);
+		int sum = 0;
+		
+		while (scanner.hasNext()) {
+			sum += scanner.nextInt();
 		}
-		System.out.println(total);
+		System.out.println(sum);
+		
+		scanner.close();
 	}
-
 }
