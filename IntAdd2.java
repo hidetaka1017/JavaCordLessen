fcd2859782cf8dd6d808c9ee6e1ddfde8d827562
hidetaka1017 <98687395+hidetaka1017@users.noreply.github.com>
2022-05-29 12:29:38 +0900

    Add files via upload

---
+++ IntAdd2.java
@@ -0,0 +1,27 @@
+package algorizum;
+
+import java.util.Scanner;
+
+public class IntAdd2 {
+
+//	整数の足し算 ２つの整数を足す
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		System.out.println(N + M);
+		
+		sc.close();
+		
+//		Scanner scanner = new Scanner(System.in);
+//        int sum = 0;
+//
+//        while (scanner.hasNext()) {
+//            sum += scanner.nextInt();
+//        }
+//
+//        System.out.println(sum);
+//
+//        scanner.close();
+	}
+}
