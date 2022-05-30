80a5abb9fa63d2798a13d594ca4f405ef6f85532
hidetaka1017 <98687395+hidetaka1017@users.noreply.github.com>
2022-05-30 20:31:13 +0900

    Add files via upload

---
+++ UpperCase.java
@@ -0,0 +1,13 @@
+package algorizum;
+
+import java.util.Scanner;
+
+//文字列がスペース区切りで2つ入力されるので、スペースで分割し、2行で出力
+public class Split {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextLine().toUpperCase());
+		sc.close();
+	}
+}
