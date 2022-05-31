121c22471e4152290533ee7b539eabddf3a5a1b7
hidetaka1017 <98687395+hidetaka1017@users.noreply.github.com>
2022-05-31 21:06:26 +0900

    Add files via upload

--- StringOutput12.java
+++ StringOutput12.java
@@ -11,6 +11,13 @@ public class StringOutput12 {
 		String str = S.substring(0,1) + " " + S.substring(1,2);
 		System.out.println(str);
 		
+//		public static void main(String[] args) {
+//		    Scanner scan = new Scanner(System.in);
+//		    String str[] = scan.nextLine().split("");
+//
+//		    System.out.println(str[0] + " " + str[1]);
+//		  }
+		
 		sc.close();
 	}
 }
