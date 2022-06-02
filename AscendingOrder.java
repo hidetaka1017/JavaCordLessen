c1de1c192d2151e84589d3f93a62f9e0b545a374
hidetaka1017 <98687395+hidetaka1017@users.noreply.github.com>
2022-06-02 22:15:35 +0900

    Update AscendingOrder.java

--- AscendingOrder.java
+++ AscendingOrder.java
@@ -3,6 +3,8 @@ package algorizum;
 import java.util.Arrays;
 import java.util.Scanner;
 
+//1行目で整数 n が与えられ、2行目で n 個の整数が与えられます。
+//n 個の整数を昇順に出力してください。
 public class AscendingOrder {
 
 	public static void main(String[] args) {
